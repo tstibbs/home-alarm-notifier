e4edd2edb83c34672a5e279c10fda2318070f384
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-06-12 20:24:46 +0100

    Upgrading to AWS SDK 3 + general lib upgrade

--- aws/functions/src/incomingTriggerFunction.js
+++ aws/functions/src/incomingTriggerFunction.js
@@ -1,21 +1,20 @@
-import aws from 'aws-sdk'
+import {IoT} from '@aws-sdk/client-iot'
+import {IoTDataPlane} from '@aws-sdk/client-iot-data-plane'
 
+import {defaultAwsClientConfig} from '@tstibbs/cloud-core-utils/src/tools/aws-client-config.js'
 import {INCOMING_TOPIC_NAME} from '../../../edge/app/constants.js'
 
-const iot = new aws.Iot({
-	apiVersion: '2015-05-28'
-})
+const iot = new IoT(defaultAwsClientConfig)
 const dataEndpointPromise = buildIotDataApi()
 
 async function buildIotDataApi() {
-	const endpointResponse = await iot
-		.describeEndpoint({
-			endpointType: 'iot:Data-ATS'
-		})
-		.promise()
-	const iotdata = new aws.IotData({
-		endpoint: endpointResponse.endpointAddress,
-		apiVersion: '2015-05-28'
+	const endpointResponse = await iot.describeEndpoint({
+		endpointType: 'iot:Data-ATS'
+	})
+
+	const iotdata = new IoTDataPlane({
+		endpoint: 'https://' + endpointResponse.endpointAddress,
+		...defaultAwsClientConfig
 	})
 	return iotdata
 }
@@ -23,13 +22,12 @@ async function buildIotDataApi() {
 export async function handler(event, context) {
 	let iotdata = await dataEndpointPromise //workaround until we can use top level await
 	console.log('sending message...')
-	await iotdata
-		.publish({
-			topic: INCOMING_TOPIC_NAME,
-			payload: null,
-			qos: '1'
-		})
-		.promise()
+	await iotdata.publish({
+		topic: INCOMING_TOPIC_NAME,
+		payload: null,
+		qos: '1'
+	})
+
 	console.log('sent message.')
 	return {
 		statusCode: '200'
